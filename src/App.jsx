50ea5eb564a0ec326188104b93e618102cbf050b
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-03 18:58:21 +0330

    src ap.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
-import './App.css'
+import style from "./App.module.css"
 
 function App() {
+  const age= 20;
+  if(age > 20) console.log("over age")
+  else console.log("under age")
+
+  age > 20 ? console.loge("over age") : console.log("under age")
   return (
-    <div>
-     <User name="Aida" age={21} email="aida.nknjd@gmail.com"/>
-     <User />
-     <User />
+    <div className="App">
+      AidaNiknejad
     </div>
   )
 }
-
-const GetName = () => {
-  return "Aida"
-}
-//props 
-// const props = {
-//   name : "Aida",
-//   age : 21,
-//   email : "aida.nknjd@gmail.com"
-
-// }
-
-
-
-//component 
-const User = (props) =>{
-  return (
-    <div>
-  <h1>{props.name}</h1>
-  <h2>{props.age}</h2>
-  <h2>{props.email}</h2>
-  </div>
-  )
-}
-
-export default App
+export default App;
