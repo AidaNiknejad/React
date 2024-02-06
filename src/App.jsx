3263da67b78280e502bf7a995195108d551b49da
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-06 18:13:10 +0330

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
 import "./App.css"
 function App() {
+  const [courseList , setCourseList] = useState([]);
 
-}
+  const [newCourse , setNewCourse] = useState("");
+  
+  const handelChange =(event)=>{
+
+    setNewCourse(event.target.value);
+
+  }
+
+  const addCourse =()=>{
+    setCourseList([...courseList , newCourse])
+    console.log(courseList)
+  }
+  const deleteCourse =(courseName) =>{
+  const newCourseList = courseList.filter((course)=>{courseName !== course
+  })
+  setCourseList(newCourseList);
+  };
     
   return (
       <div className="App">
         <div className="add-course">
-          <input type = "text"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           >
+          <input type="text" onChange={handelChange}  placeholder="Enter course name"> </input>   
+          <button onClick={addCourse}>Add Course</button>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
         </div>
+        {newCourse}
         <div className="list"></div>
+        { courseList.map ((course , index) =>{
+        return (<div key={index}>
+        <h1> {course} </h1>
+        <button  onClick={()=> deleteCourse(course)}>x</button>
+        </div>
+        )
+        })}
       </div>
   );
 } 
