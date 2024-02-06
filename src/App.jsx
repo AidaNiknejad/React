d263ad025e65b9e2eb761a47d8d5b303fc25bb09
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-06 14:19:15 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import "./App.css"
-import {useState} from 'react'6
 function App() {
-const [count , setCount] = useState (0);
-const increase = () => {
-  setCount(count +1);
-};
-const decrease = () => {
-  setCount(count - 1);
-};
-const setZero = () => {
-  setCount(0);
-};
+
+}
     
-  return <div className="App">
-    <button onClick={increase}>increase</button>
-    <button onClick={decrease}>decrease</button>
-    <button onClick={setZero}>set to 0</button>
-    <h1>{count}</h1>
-    </div>
-      
+  return (
+      <div className="App">
+        <div className="add-course">
+          <input type = "text"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           >
+        </div>
+        <div className="list"></div>
+      </div>
+  );
 } 
 export default App;
 
