46b3e44a8b1201fc2544d7488fd67748c4f1c2bd
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-07 18:55:46 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,46 @@
 import { useState } from "react";
-import "./App.css"
+import "./App.css";
 function App() {
   const [courseList , setCourseList] = useState([]);
-
   const [newCourse , setNewCourse] = useState("");
-  
-  const handelChange =(event)=>{
 
+
+  const handelChange=(event)=>{
     setNewCourse(event.target.value);
 
-  }
-
-  const addCourse =()=>{
-    setCourseList([...courseList , newCourse])
-    console.log(courseList)
-  }
-  const deleteCourse =(courseName) =>{
-  const newCourseList = courseList.filter((course)=>{courseName !== course
-  })
-  setCourseList(newCourseList);
   };
-    
+ const addCourse = ()=>{
+const course = {
+  id : courseList.length === 0 ? 1 : courseList[courseList.length-1].id + 1 ,
+  courseName : newCourse
+}
+
+  setCourseList( [...courseList , course]);
+   console.log(courseList);
+}
+  const deleteCourse =(courseName)=>{
+    const newCourseList = courseList.filter((course)=>{
+      if(course === courseName) return false
+      else return true
+    })
+    setCourseList(newCourseList)
+  }
   return (
       <div className="App">
         <div className="add-course">
-          <input type="text" onChange={handelChange}  placeholder="Enter course name"> </input>   
-          <button onClick={addCourse}>Add Course</button>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+          <input type="text" onChange={handelChange} />
+          <button onClick={addCourse}>Add Course</button>
         </div>
-        {newCourse}
-        <div className="list"></div>
-        { courseList.map ((course , index) =>{
-        return (<div key={index}>
-        <h1> {course} </h1>
-        <button  onClick={()=> deleteCourse(course)}>x</button>
+        <div className="list">
+          {courseList.map((course)=>{
+            return (
+            <div>
+              <h1>{course.courseName}</h1>
+              <button  onClick={() => deleteCourse(course)}>x</button>
+            </div>
+            )
+          })}
         </div>
-        )
-        })}
       </div>
   );
 } 
