8b1d3dd4d143638057ac4ffd6afc32e74b022b18
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-17 15:47:15 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-import Text from "./Text"
-
-function App() {
-   const [showText , setShowText] = useState(false)
-  return (
-      <div className='App'>
-        <button onClick={()=> setShowText (!showText)}>show Text</button>
-        {showText && <Text />}
-      </div>
-  )
-} 
-export default App; 
+export default function App() {
+  return( <div>
+    <h1>Hello World</h1>
+    <button>get advice</button>
+  </div>
+  );
+}
