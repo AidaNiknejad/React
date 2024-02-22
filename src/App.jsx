3b768d87e7d466331c0a179e26111f3cb7aebc1e
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-22 16:57:20 +0330

    import home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {Home}
+import { Home } from "./Pages/Home";
 function App() {
   return (
     <div className="App">
