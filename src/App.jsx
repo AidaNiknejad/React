71cc85cac227b7150241960ed1aaf89c3e3abb86
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-23 20:19:12 +0330

    create simple menu with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
+import { Nav } from "./pages/Nav";
 function App() {
   return (
     <div className="App">
       <Router>
+        <div>SarvinStyle</div>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<div>Not found</div>} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Nav.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export const Nav = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link> |<Link to="/about">About</Link> |
+      <Link to="/contact">Contact me</Link>
+    </nav>
+  );
+};
