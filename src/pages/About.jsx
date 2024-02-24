c0a95c00417ebf4669f8c3af185e402df08a37ce
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-24 14:27:10 +0330

    navigation
    modify useNavigation in button tag

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,17 @@
-export const About=()=>{
-    return <div>This is about page</div>
-}
+import React from "react";
+import { useNavigate } from "react-router-dom";
+export const About = () => {
+  let navigate = useNavigate();
+  return (
+    <React.Fragment>
+      <div>This is about page</div>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go to home page
+      </button>
+    </React.Fragment>
+  );
+};
