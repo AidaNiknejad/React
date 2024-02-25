90dbf78dc7dbaac38500ef628e50843e843b4bb9
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-25 16:22:53 +0330

    props

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,3 @@
-import { useParams } from "react-router-dom";
-export const Profile = () => {
-  const { name } = useParams();
-  return <div>This is profile page of {name}</div>;
+export const Profile = (props) => {
+  return <div>This is profile page - username is {props.username}</div>;
 };
